1a13db63f69a0a8d1dd4ac7b6f2ef99266dccb6d
Milad <smiladhi@gmail.com>
2017-03-12 21:29:50 +1100

    Added HostListener

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,23 +1,24 @@
-import {Component, Input, OnInit, OnChanges, SimpleChange, SimpleChanges, DoCheck, OnDestroy} from "@angular/core";
+import {
+    Component, Input, OnInit, OnChanges, SimpleChange, SimpleChanges, DoCheck, OnDestroy,
+    HostListener
+} from "@angular/core";
 
 @Component({
     selector: 'feed',
     templateUrl: './feed.component.html',
     styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnDestroy {
+export class FeedComponent {
     @Input('text') text;
     private feed: number;
 
     constructor() {
         this.feed = Math.floor(Math.random() * 100);
-        console.log('Creating FeedComponent with feed#' + this.feed);
     }
 
-    ngOnDestroy(): void {
-        console.log(`FeedComponent feed #${this.feed} about to be destroyed`);
+    @HostListener('click', ['$event','feed']) onClick($event,feed) {
+        console.log('FeedComponent click $event', $event,feed);
     }
-
 
     get randomFeed(): string {
         return this.text || 'feed #' + this.feed;
