8823e4c44e7a8613f5ad1dc73908ca903e2e1379
Milad <smiladhi@gmail.com>
2017-03-12 20:14:10 +1100

    Adde click output

--- src/app/components/feeds/feeds.component.ts
+++ src/app/components/feeds/feeds.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'feeds',
-  templateUrl: './feeds.component.html',
-  styleUrls: ['./feeds.component.css']
+    selector: 'feeds',
+    templateUrl: './feeds.component.html',
+    styleUrls: ['./feeds.component.css']
 })
-export class FeedsComponent implements OnInit {
+export class FeedsComponent {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    onClick() {
+        alert('You clicked on a feed');
+    }
 
 }
